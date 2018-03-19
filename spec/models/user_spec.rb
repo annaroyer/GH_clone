fb81242b213229505a531a6eac495ca8f21ab1a5
Anna Royer <anna.royer@colorado.edu>
2018-03-19 14:27:12 -0600

    add test for user update_or_create method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+def auth_hash
+    {
+    uid: 26782839,
+    info: {
+      nickname: "annaroyer",
+      email: "anna.royer@colorado.edu",
+      name: "Anna Royer",
+      image: "https://avatars0.githubusercontent.com/u/26782839?v=4",
+      urls: {"GitHub"=>"https://github.com/annaroyer", "Blog"=>""}
+    },
+    credentials: {token: "5f13e5a5356568ef391219247a05758311d4c5bd", "expires"=>false}
+  }
+end
+
+describe User, type: :model do
+  context 'class methods' do
+    describe '.update_or_create' do
+      it 'creates or updates a user with the auth hash' do
+        user = User.update_or_create(auth_hash)
+
+        expect(user.uid).to eq(auth_hash[:uid])
+        expect(user.email).to eq(auth_hash[:info][:email])
+      end
+    end
+  end
 end
