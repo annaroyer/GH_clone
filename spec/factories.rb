8b75b1a1ca9cfbfaaa2f6f2f57234a2b92cfc967
Anna Royer <anna.royer@colorado.edu>
2018-03-19 14:36:27 -0600

    add test user can see basic information

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    uid 26782839
+    email "anna.royer@colorado.edu"
+    nickname "annaroyer"
+    name "Anna Royer"
+    image "https://avatars0.githubusercontent.com/u/26782839?v=4"
+    token "5f13e5a5356568ef391219247a05758311d4c5bd"
   end
-
-
 end
