de6f3ba454fa38178b3364c1090fda365352d32b
Anna Royer <anna.royer@colorado.edu>
2018-03-21 12:48:19 -0600

    refactor?

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,15 +1,21 @@
 class GithubService < ApplicationController
-  attr_reader :conn
-
   def initialize(access_token)
-    @conn = Faraday.new do |faraday|
-      faraday.headers[:authorization] = "token #{access_token}"
-      faraday.adapter Faraday.default_adapter
-    end
+    @access_token = access_token
   end
 
   def get_url(url)
-    response = @conn.get("https://api.github.com/#{url}")
+    response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  private
+    attr_reader :access_token
+
+    def conn
+      Faraday.new('https://api.github.com', headers: headers)
+    end
+
+    def headers
+      { authorization: "token #{access_token}" }
+    end
 end
