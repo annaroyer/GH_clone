cfe831a5339616f27f8a6ec33a24c8975d1d305b
Anna Royer <anna.royer@colorado.edu>
2018-03-21 15:55:26 -0600

    add webmock to test

--- spec/features/user_can_log_in_with_github_spec.rb
+++ spec/features/user_can_log_in_with_github_spec.rb
@@ -13,7 +13,7 @@ def stub_omniauth
       "image"=>"https://avatars0.githubusercontent.com/u/26782839?v=4",
       "urls"=>{"GitHub"=>"https://github.com/annaroyer", "Blog"=>""}
     },
-    "credentials"=>{"token"=>ENV['ACCESS_TOKEN']},
+    "credentials"=>{"token"=>12345},
     "extra"=>
     {
       "raw_info"=>
@@ -35,13 +35,11 @@ describe 'As a user' do
       json_body = File.open('./spec/fixtures/starred_repos.json')
       stub_request(:get, "https://api.github.com/user/starred")
          .to_return(status: 200, body: json_body, headers: {})
+
       stub_omniauth
       visit root_path
 
       click_link 'Sign In with Github'
-
-      user = User.find_by(uid: 26782839)
-      github_user = GithubUser.new(user)
 
       expect(page).to have_link('Log Out')
       expect(current_path).to eq('/annaroyer')
@@ -51,13 +49,13 @@ describe 'As a user' do
       expect(page).to have_link('anna.royer@colorado.edu')
       expect(page).to have_content('Student')
       expect(page).to have_content('Boulder, CO')
-      expect(page).to have_xpath("//img[contains(@src, '#{github_user.image}')]")
+      expect(page).to have_xpath("//img[contains(@src, 'https://avatars0.githubusercontent.com/u/26782839?v=4')]")
 
       expect(page).to have_button('Overview')
-      expect(page).to have_button("Repositories #{github_user.repo_count}")
-      expect(page).to have_button("Followers #{github_user.follower_count}")
-      expect(page).to have_button("Following #{github_user.following_count}")
-      expect(page).to have_button("Stars #{github_user.starred_repos.count}")
+      expect(page).to have_button("Repositories 39")
+      expect(page).to have_button("Followers 4")
+      expect(page).to have_button("Following 4")
+      expect(page).to have_button("Stars 1")
     end
   end
 end
