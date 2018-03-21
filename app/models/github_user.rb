afaef64e3d5e1bad351dc1f20943c1642e07bbc5
Anna Royer <anna.royer@colorado.edu>
2018-03-21 12:38:23 -0600

    begin writing mapping to make repos

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,5 +1,13 @@
 class GithubUser < SimpleDelegator
-  def star_count
-    GithubService.new(self.token).get_url('starred').count
+  def github_service
+    GithubService.new(token)
+  end
+
+  def starred_repos
+    # github_service.get_url('user/starred').map do |raw_repo|
+    #   Repo.new(raw_repo)
+    # end
+
+    github_service.get_url('user/starred')
   end
 end
