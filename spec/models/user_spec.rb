26386986535d620dd091070f9db8a90417f45e07
Anna Royer <anna.royer@colorado.edu>
2018-03-20 20:48:12 -0600

    update auth hash

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
 def auth_hash
-    {
-    uid: 26782839,
-    info: {
-      nickname: "annaroyer",
-      email: "anna.royer@colorado.edu",
-      name: "Anna Royer",
-      image: "https://avatars0.githubusercontent.com/u/26782839?v=4",
-      urls: {"GitHub"=>"https://github.com/annaroyer", "Blog"=>""}
-    },
-    credentials: {token: "5f13e5a5356568ef391219247a05758311d4c5bd", "expires"=>false}
+  {uid: "26782839",
+   info: {
+     nickname: "annaroyer",
+     email: "anna.royer@colorado.edu",
+     name: "Anna Royer",
+     image: "https://avatars0.githubusercontent.com/u/26782839?v=4"
+   },
+   credentials: {token: "ABC2875902487509"},
+   extra:
+    {raw_info:
+      {
+       location: "Boulder, CO",
+       bio: "Student",
+       public_repos: 39,
+       followers: 4,
+       following: 4,
+       total_private_repos: 0
+      }
+    }
   }
 end
 
@@ -20,7 +29,7 @@ describe User, type: :model do
       it 'creates or updates a user with the auth hash' do
         user = User.update_or_create(auth_hash)
 
-        expect(user.uid).to eq(auth_hash[:uid])
+        expect(user.token).to eq(auth_hash[:credentials][:token])
         expect(user.email).to eq(auth_hash[:info][:email])
       end
     end
