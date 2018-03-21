5478ae2da143f3734d06a58be0adc6b1d0d1f9f2
Anna Royer <anna.royer@colorado.edu>
2018-03-21 15:24:21 -0600

    add basic information and add environment variable for token

--- spec/features/user_can_log_in_with_github_spec.rb
+++ spec/features/user_can_log_in_with_github_spec.rb
@@ -13,7 +13,7 @@ def stub_omniauth
       "image"=>"https://avatars0.githubusercontent.com/u/26782839?v=4",
       "urls"=>{"GitHub"=>"https://github.com/annaroyer", "Blog"=>""}
     },
-    "credentials"=>{"token"=>"78970ajhlkjh"},
+    "credentials"=>{"token"=>ENV['ACCESS_TOKEN']},
     "extra"=>
     {
       "raw_info"=>
@@ -29,14 +29,32 @@ def stub_omniauth
   })
 end
 
-feature 'user logs in' do
-  scenario 'using github' do
-    stub_omniauth
-    visit root_path
+describe 'As a user' do
+  context 'When  I log in using github' do
+    scenario 'I can see my basic info' do
+      stub_omniauth
+      visit root_path
 
-    click_link 'Sign In with Github'
+      click_link 'Sign In with Github'
 
-    expect(page).to have_content('Anna Royer')
-    expect(page).to have_link('Log Out')
+      user = User.find_by(uid: 26782839)
+      github_user = GithubUser.new(user)
+
+      expect(page).to have_link('Log Out')
+      expect(current_path).to eq('/annaroyer')
+      
+      expect(page).to have_content('Anna Royer')
+      expect(page).to have_content('annaroyer')
+      expect(page).to have_link('anna.royer@colorado.edu')
+      expect(page).to have_content('Student')
+      expect(page).to have_content('Boulder, CO')
+      expect(page).to have_xpath("//img[contains(@src, '#{github_user.image}')]")
+
+      expect(page).to have_button('Overview')
+      expect(page).to have_button("Repositories #{github_user.repo_count}")
+      expect(page).to have_button("Followers #{github_user.follower_count}")
+      expect(page).to have_button("Following #{github_user.following_count}")
+      expect(page).to have_button("Stars #{github_user.starred_repos.count}")
+    end
   end
 end
