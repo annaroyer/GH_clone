1ec99df68f39ca06cd06aff3f8a4f8eaed288594
Anna Royer <anna.royer@colorado.edu>
2018-03-20 21:51:56 -0600

    update headers to connect correctly

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,13 +1,15 @@
 class GithubService < ApplicationController
-  def initialize(token)
-    @conn = Faraday.new("https://api.github.com/user") do |faraday|
-      faraday.headers[:access_token] = token
+  attr_reader :conn
+
+  def initialize(access_token)
+    @conn = Faraday.new do |faraday|
+      faraday.headers[:authorization] = "token #{access_token}"
       faraday.adapter Faraday.default_adapter
     end
   end
 
   def get_url(url)
-    response = @conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)[:results]
+    response = @conn.get("https://api.github.com/user/#{url}")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
