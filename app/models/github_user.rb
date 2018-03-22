8aeaf0819b668e6f704acdc84ae42c3ba4a46cca
Anna Royer <anna.royer@colorado.edu>
2018-03-22 13:40:48 -0600

    move event methods over to github user

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -7,17 +7,26 @@ class GithubUser < SimpleDelegator
     github_service.get_url('user/starred')
   end
 
-  def events
-    Event.find_by_user(nickname)
+  def push_events
+    get_push_events.map do |raw_event|
+      PushEvent.new(raw_event)
+    end
   end
 
   def recent_commits
-    events.sum(&:commit_count)
+    push_events.sum(&:commit_count)
   end
 
   def recent_repo_commits
-    events.group_by(&:repo_name).transform_values do |events|
+    push_events.group_by(&:repo_name).transform_values do |events|
       events.sum(&:commit_count)
     end
   end
+
+  private
+    def get_push_events
+      github_service.get_url("users/#{nickname}/events").find_all do |event|
+        event[:type] == 'PushEvent'
+      end
+    end
 end
