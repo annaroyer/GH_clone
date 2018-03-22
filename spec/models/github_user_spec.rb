597690b47c3ea16b08d5fe125cf591899e9df198
Anna Royer <anna.royer@colorado.edu>
2018-03-21 22:55:04 -0600

    add test

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe GithubUser do
-  
+  context 'instance methods' do
+    describe '#events' do
+      it "returns a collection of a user's events" do
+        
+      end
+    end
+  end
 end
