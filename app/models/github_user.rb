ee658a0b5c9687108ee24a896116a46077b903ad
Anna Royer <anna.royer@colorado.edu>
2018-03-21 22:50:27 -0600

    add methods to interact with events from view - need to refactor these..

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,13 +1,23 @@
-class GithubUser < SimpleDelegator    
+class GithubUser < SimpleDelegator
   def github_service
     GithubService.new(token)
   end
 
   def starred_repos
-    # github_service.get_url('user/starred').map do |raw_repo|
-    #   Repo.new(raw_repo)
-    # end
-
     github_service.get_url('user/starred')
+  end
+
+  def events
+    Event.find_by_user(nickname)
+  end
+
+  def recent_commits
+    events.sum(&:commit_count)
+  end
+
+  def recent_repo_commits
+    events.group_by(&:repo_name).transform_values do |events|
+      events.sum(&:commit_count)
+    end
   end
 end
