f807032d018a933b838358c512adf15184d6773e
Anna Royer <anna.royer@colorado.edu>
2018-03-22 19:12:53 -0600

    add index method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @github_user = GithubUser.new(current_user)
   end
+
+  def index
+    @github_user = GithubUser.new(current_user)
+  end
 end
