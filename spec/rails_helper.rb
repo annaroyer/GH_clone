a95fc0dd7926be541a7a8051dbf07f465427973e
Anna Royer <anna.royer@colorado.edu>
2018-03-22 19:15:18 -0600

    change vcr configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,5 +22,6 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/cassettes"
   config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
   config.filter_sensitive_data('<ACCESS_TOKEN>') { ENV['ACCESS_TOKEN'] }
 end
