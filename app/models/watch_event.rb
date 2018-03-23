504179f782499376b01cbbce11da04306cb97a6d
Anna Royer <anna.royer@colorado.edu>
2018-03-22 19:05:06 -0600

    add watch event model

---
+++ app/models/watch_event.rb
@@ -0,0 +1,22 @@
+class WatchEvent
+  attr_reader :user_avatar,
+              :created_at
+              
+  def initialize(attrs={})
+    @user_name = attrs[:actor][:login]
+    @user_avatar = attrs[:actor][:avatar_url]
+    @repo_name = attrs[:repo][:name]
+    @action = attrs[:payload][:action]
+    @created_at = attrs[:created_at]
+  end
+
+  def summary
+    "#{user_name} #{action} watching #{repo_name}"
+  end
+
+  private
+    attr_reader :user_name,
+                :repo_name,
+                :action
+
+end
