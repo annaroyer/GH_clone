ba20fd2e88d93deb1e1048373aa7d2915855bc64
Anna Royer <anna.royer@colorado.edu>
2018-03-23 06:37:40 -0600

    add simple cov start

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,12 @@
+require 'simplecov'
+SimpleCov.start 'rails'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'webmock/rspec'
+
 
 ActiveRecord::Migration.maintain_test_schema!
 
