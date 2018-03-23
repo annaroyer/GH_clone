0469b59c4ebad5f9ac0227981bcb9bff19f24e68
Anna Royer <anna.royer@colorado.edu>
2018-03-22 22:49:25 -0600

    test all commit event methods

--- spec/models/fork_event_spec.rb
+++ spec/models/fork_event_spec.rb
@@ -6,18 +6,18 @@ describe ForkEvent do
 
   context 'instance methods' do
     describe '#summary' do
-      it 'returns a summary of the event' do
+      xit 'returns a summary of the event' do
         expect(subject.summary).to eq('slimecog started watching slimecog/black_thursday')
       end
     end
   end
 
   context 'attributes' do
-    it 'has a user avatar' do
+    xit 'has a user avatar' do
       expect(subject.user_avatar).to eq("https://avatars.githubusercontent.com/u/7215067?")
     end
 
-    it 'has a created_at' do
+    xit 'has a created_at' do
       expect(subject.created_at).to eq("2018-03-08T18:30:12Z")
     end
   end
